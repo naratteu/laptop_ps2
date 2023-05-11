563ae3da7d3ffebb5fbc73b00e11f1057eac2d92
박성원 <skfhddlg@naver.com>
2023-05-12 00:41:17 +0900

    코드정리, irq_num처리 옮김.

--- int_pins.h
+++ int_pins.h
@@ -13,14 +13,14 @@
   #define CORE_INT5_PIN  18
 
 // Arduino Due (untested)
-#elif defined(__SAM3X8E__) 
-  #define CORE_INT_EVERY_PIN
-  #ifndef PROGMEM
-  #define PROGMEM
-  #endif
-  #ifndef pgm_read_byte
-  #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
-  #endif
+#elif defined(__SAM3X8E__)
+#define CORE_INT_EVERY_PIN
+#ifndef PROGMEM
+#define PROGMEM
+#endif
+#ifndef pgm_read_byte
+#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+#endif
 
 // Arduino Leonardo (untested)
 #elif defined(__AVR_ATmega32U4__) && !defined(CORE_TEENSY)
@@ -60,15 +60,15 @@
 
 //ESP8266 - NodeMCU 0.9 (ESP-12 Module) / NodeMCU 1.0 (ESP-12E Module)
 #elif defined(ESP8266)
-  #define CORE_INT_EVERY_PIN
+#define CORE_INT_EVERY_PIN
 
 //ESP32 support
 #elif defined(ESP32)
-  #define CORE_INT_EVERY_PIN
+#define CORE_INT_EVERY_PIN
 
 //STM32F1 support
 #elif defined(__STM32F1__)
-  #define CORE_INT_EVERY_PIN
+#define CORE_INT_EVERY_PIN
 
 // Arduino Uno, Duemilanove, LilyPad, Mini, Fio, etc...
 #else
@@ -77,4 +77,136 @@
 
 #endif
 #endif
+uint8_t get_irq_num(uint8_t irq_pin) {
+  uint8_t irq_num = 255;
 
+
+#ifdef CORE_INT_EVERY_PIN
+  irq_num = irq_pin;
+
+#else
+  switch (irq_pin) {
+#ifdef CORE_INT0_PIN
+    case CORE_INT0_PIN:
+      irq_num = 0;
+      break;
+#endif
+#ifdef CORE_INT1_PIN
+    case CORE_INT1_PIN:
+      irq_num = 1;
+      break;
+#endif
+#ifdef CORE_INT2_PIN
+    case CORE_INT2_PIN:
+      irq_num = 2;
+      break;
+#endif
+#ifdef CORE_INT3_PIN
+    case CORE_INT3_PIN:
+      irq_num = 3;
+      break;
+#endif
+#ifdef CORE_INT4_PIN
+    case CORE_INT4_PIN:
+      irq_num = 4;
+      break;
+#endif
+#ifdef CORE_INT5_PIN
+    case CORE_INT5_PIN:
+      irq_num = 5;
+      break;
+#endif
+#ifdef CORE_INT6_PIN
+    case CORE_INT6_PIN:
+      irq_num = 6;
+      break;
+#endif
+#ifdef CORE_INT7_PIN
+    case CORE_INT7_PIN:
+      irq_num = 7;
+      break;
+#endif
+#ifdef CORE_INT8_PIN
+    case CORE_INT8_PIN:
+      irq_num = 8;
+      break;
+#endif
+#ifdef CORE_INT9_PIN
+    case CORE_INT9_PIN:
+      irq_num = 9;
+      break;
+#endif
+#ifdef CORE_INT10_PIN
+    case CORE_INT10_PIN:
+      irq_num = 10;
+      break;
+#endif
+#ifdef CORE_INT11_PIN
+    case CORE_INT11_PIN:
+      irq_num = 11;
+      break;
+#endif
+#ifdef CORE_INT12_PIN
+    case CORE_INT12_PIN:
+      irq_num = 12;
+      break;
+#endif
+#ifdef CORE_INT13_PIN
+    case CORE_INT13_PIN:
+      irq_num = 13;
+      break;
+#endif
+#ifdef CORE_INT14_PIN
+    case CORE_INT14_PIN:
+      irq_num = 14;
+      break;
+#endif
+#ifdef CORE_INT15_PIN
+    case CORE_INT15_PIN:
+      irq_num = 15;
+      break;
+#endif
+#ifdef CORE_INT16_PIN
+    case CORE_INT16_PIN:
+      irq_num = 16;
+      break;
+#endif
+#ifdef CORE_INT17_PIN
+    case CORE_INT17_PIN:
+      irq_num = 17;
+      break;
+#endif
+#ifdef CORE_INT18_PIN
+    case CORE_INT18_PIN:
+      irq_num = 18;
+      break;
+#endif
+#ifdef CORE_INT19_PIN
+    case CORE_INT19_PIN:
+      irq_num = 19;
+      break;
+#endif
+#ifdef CORE_INT20_PIN
+    case CORE_INT20_PIN:
+      irq_num = 20;
+      break;
+#endif
+#ifdef CORE_INT21_PIN
+    case CORE_INT21_PIN:
+      irq_num = 21;
+      break;
+#endif
+#ifdef CORE_INT22_PIN
+    case CORE_INT22_PIN:
+      irq_num = 22;
+      break;
+#endif
+#ifdef CORE_INT23_PIN
+    case CORE_INT23_PIN:
+      irq_num = 23;
+      break;
+#endif
+  }
+#endif
+  return irq_num;
+}
